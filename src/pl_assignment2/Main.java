69fb23d03350d2e7e5774c90af5c1dbf01df837d
Kchanghun <abcd877287@gmail.com>
2021-12-12 20:02:22 +0900

    add path

--- src/pl_assignment2/Main.java
+++ src/pl_assignment2/Main.java
@@ -1,10 +1,37 @@
 package pl_assignment2;
 
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("hi");
+		Path path;
+		if(args[0].equals("-v")) {
+			path= Paths.get(args[1]);
+		}
+		else{
+			path= Paths.get(args[0]);
+		}
+		Charset charset=Charset.forName("UTF-8");
+		List<String> lines=null;
+		try {
+			lines=Files.readAllLines(path,charset);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	
+		String context="";
+		int[] sizeOfLine=new int[lines.size()];
+		for(int i=0;i<lines.size();i++) {
+			context+=lines.get(i)+" ";
+			sizeOfLine[i]=context.length()-1;
+		}
+		
 	}
 
 }
